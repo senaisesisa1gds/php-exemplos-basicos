9055fe9c50d71fc72946ee6b2f06d37721f613db
senaisesisa1gds <senaisesisa1igds@gmail.com>
2026-08-25 16:08:29 -0300

    Completo
    Exemplo 1 feito.

--- Assunto 2 - Protocolo HTTP/1_cadastro_status.php
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -18,6 +18,43 @@
 
     </form>
 
+    <!-- Linha horizontal -->
+    <hr>
+
+    <?php
+    // $_SERVER é uma variável superglobal do PHP que contém informações sobre requisições feitas pelo servidor. Aqui ele esta confirmando se a requisição é via POST (Protocolo HTTP)
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Se sim ele acessa as informações (Nome e idade)
+        $nome = $_POST["nome"];
+        $idade = $_POST["idade"];
+
+        // Tratando erros e interagindo.
+
+        // Erro: Não prencheu nome e idade.
+        if ($nome == "" || $idade == "") {
+          http_response_code(400);
+          echo "<h2>Status 400 - Faltou nome ou idade</h2>";
+
+        // Não digitou um número. Ex: Digitou "vinte" e não o número 20.  
+        } elseif (!is_numeric($idade)) {
+            http_response_code(400);
+            echo "<h2>Status 400 - Idade precisa ser um número</h2>";
+
+        // Preenchidos corretamente (Todos dados)
+        } else {
+            http_response_code(201);
+            echo "<h2>Status 201 - Criado: $nome, $idade anos.</h2>";
+        }
+
+    } else {
+        // Usuário ainda não enviou nada.
+        http_response_code(200);
+        echo "<p> Preencha o formulário acima e envie.</p>";
+    }
+
+
+    ?>
+
     
 </body>
 </html>
